c4de97425b92774b01986dcd7b5caca1b7795ec0
Janet <janetkarpenske@gmail.com>
2020-06-16 13:57:17 -0700

    Add function to calculate the cost of tickets

--- js/scripts.js
+++ js/scripts.js
@@ -17,13 +17,38 @@ CustomerPurchases.prototype.assignId = function(customerTicket) {
 
 CustomerPurchases.prototype.findTicket = function(id) {
   for (let i=0; i<this.customerTickets.length; i++) {
-    if (this.customerTickets[i].id == id) {
-      return this.customerTickets[i];
+    if (this.CustomerTickets[i]) {
+      if (this.customerTickets[i].id == id) {
+        return this.customerTickets[i];
+      }
     }
   };
   return false;
 }
 
+CustomerPurchases.prototype.deleteTicket = function(id) {
+  for (let i=0; i<this.customerTickets.length; i++) {
+    if (this.CustomerTickets[i]) {
+      if (this.customerTickets[i].id == id) {
+        delete this.customerTickets[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+CustomerPurchases.prototype.calcTicket = function(customerTicket) {
+  if (rerun > 0) {
+    rerun * 8
+  }
+  if (matinee > 0) {
+    matinee * 10
+  }
+  if (senior > 0) {
+    senior * 6
+  }
+}
 
 //Business Logic for handling Tickets/Types of Tickets (CustomerTicket):
 
